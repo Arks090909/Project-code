3d98d7450ff6cb4cbacbda743bf174f391e6afd8
Arks090909 <meankita95@gmail.com>
2018-09-04 20:04:58 +0530

    toggle

--- js/t..mobile.js
+++ js/t..mobile.js
@@ -1,7 +1,7 @@
  function opencloseSlideMenu(){
-    	var element=document.getElementById("side-menu");
-    	element.style.display=="block"?element.style.display="none":element.style.display="block";
-    	document.getElementById("side-menu").style.width='250px';
+      var element=document.getElementById("side-menu");
+      element.style.display=="block"?element.style.display="none":element.style.display="block";
+      document.getElementById("side-menu").style.width='250px';
     }
     
  
@@ -42,4 +42,44 @@ function closeSlideMenu(){
   document.getElementById("main-content").style.width='0';
 }
 
+      var head = document.getElementById("starwar");
+var btn = head.getElementsByClassName("sam");
+for (var i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("actives");
+    if (current.length > 0) { 
+      current[0].className = current[0].className.replace(" actives", "");
+    }
+    this.className += " actives";
+  });
+}
 
+
+var heade = document.getElementById("mychoice");
+var btnns = heade.getElementsByClassName("hot-pink");
+for (var i = 0; i < btnns.length; i++) {
+  btnns[i].addEventListener("click", function(event) {
+    var popUp = event.target.querySelector("div");
+    console.dir(popUp);
+    if(popUp.className != "show"){
+       popUp.className = "show";
+    }else if(popUp.className = "show"){
+       popUp.className = "popuptext";
+    }
+   
+
+
+
+
+    // var currents = document.getElementsByClassName("popuptext");
+    //   for(var i=0;i<currents.length;i++){
+    //  if(currents[i].style.display=="none"){
+    //   this.className += " show";
+    //   }
+    //   else
+    //     currents[i].style.display="none";
+    //   }
+
+  });
+
+}
